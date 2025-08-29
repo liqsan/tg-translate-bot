670dd04ba2fd5df0970464ee75249df750e20eaf
liqsan <abramova.evgenia.00@mail.ru>
2025-08-30 00:25:03 +0300

    Запуск нового бота, добавила логи, скрыла токен в .env, исправила баги

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import telebot  
+import os  
+import logging
+from dotenv import load_dotenv
+load_dotenv()
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 
-BOT_TOKEN = "8208913505:AAGVd4_RiC3wG2MtpGkiO64G-Sn5sVOazJM"
+BOT_TOKEN=os.getenv("token")
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    logging.info(f"Получена команда /start от {message.from_user.id} ({message.from_user.username})")
     bot.send_message(message.chat.id, "Привет! Я эхо-бот. Напиши что-нибудь.")
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text','photo','video','document','audio','voice','sticker','animation','video_note'])
 def echo(message):
-    bot.send_message(message.chat.id, message.text)
+    ct = message.content_type
+    try: 
+        print(ct)
+        if ct == 'text':
+            bot.send_message(message.chat.id, message.text)
+        else:
+            bot.send_message(message.chat.id, f"Я пока не умею обрабатывать {ct}")
+    except Exception:
+        bot.send_message(message.chat.id, f"Ошибка {ct}")
+    logging.info(f"Получено сообщение: {message.text} от {message.from_user.id} ({message.from_user.username})")
 
-print("бот запущен")
+logging.info("Бот запущен и слушает сообщения...")
+
 bot.polling()
